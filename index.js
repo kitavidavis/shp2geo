2a991d8c7795fdb90c9cd304659c90a51926954f
David Kitavi <geospatialeng2017@gmail.com>
2022-08-19 11:57:33 +0300

    changes

--- index.js
+++ index.js
@@ -2,16 +2,8 @@ module.exports = function parseShapefile(url) {
     const L = require("leaflet");
     const shp = require("shpjs");
 
-    const retrieveExtension = () => {
-        return url.substring(url.lastIndexOf('.')+1, url.length) || url;
-    }
-
     if (typeof url !== "string"){
         throw new TypeError("URL must be a string")
-    }
-
-    if(retrieveExtension() !== ".zip"){
-        throw new TypeError("The shapefile must be a zip file")
     }
 
     const geo = L.geoJSON(
